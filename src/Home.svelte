488053798532466943c00e4f95220baab1c03c28
peepo5 <72892531+peepo5@users.noreply.github.com>
2023-02-09 00:07:53 +0000

    Updated with info

---
+++ src/Home.svelte
@@ -0,0 +1,60 @@
+<script>
+  /*
+  import SvelteSeo from "svelte-seo";
+  import NavBar from "../components/NavBar.svelte";
+  */
+</script>
+
+<!--
+<SvelteSeo
+  title="Neuroregen Collective"
+  description="The main site for the Neuroregen Collective (NRC)"
+/>
+-->
+<main>
+  <!--<NavBar/>-->
+  <div class="mid-sec" style="min-height: 100vh;">
+    <div class="low-sec">
+      <div style="display:flex; justify-content: center">
+        <a href="" target="_blank" rel="noreferrer"> 
+          <img src="/images/star2.png" class="logo" alt="Logo" />
+        </a>
+      </div>
+      <h2 class="py-5">We are the Neuroregen Collective</h2>
+      <p>
+        Our mission is to bring mental freedom to the masses through recovery and discovery.<br>
+        We support projects that help with our mission, and we bring recovery tools to a single place.<br>
+        !  This site is still under construction  !
+      </p>
+      <br><h2 style="color: gray;">ᐯ</h2>
+    </div>
+  </div>
+  <div class="mid-sec patterned" style="min-height: 75vh;">
+    <div class="low-sec">
+      <h2 class="py-5">About us</h2>
+      <div style="display:flex; justify-content: center">
+      <p style="max-width:550px">The Neuroregen Collective was founded in 2022. <br>
+        We exist for those who want a high level of performance and growth in their life without wasting time.<br><br>
+
+        At the moment, we are a relatively small organization, but we are networking and growing our reach to bring light to effective methods of self-growth.<br><br>
+
+        We support and create projects (and also research) that helps people become the highest performing version of themselves. On top of supporting high performers, we also support privacy, free speech and open-source software.<br><br>
+      
+        <u>New projects soon!</u><br>
+      </p>
+      </div>
+    </div>
+  </div>
+</main>
+
+<style>
+  .logo {
+    height: 200px;
+
+    padding: 0em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cff);
+  }
+</style>
